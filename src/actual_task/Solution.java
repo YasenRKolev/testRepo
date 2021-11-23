7f1d85b233a2f63b181d27662281e0f1a2ca594e
Yasen <cochise22@gmail.com>
2021-11-23 23:28:36 +0200

    add more complicated input

--- src/actual_task/Solution.java
+++ src/actual_task/Solution.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) throws IOException {
     	System.out.println("START");
 //        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String input = "{Level 1 {Level 2{Level 3-1{Level 4-1}}{Level 3-2{Level 4-2}}}{Level 2-2}}::4";
+        String input = "{Level 1 {Level 2-2{Level 3-1{Level 4-1}}{Level 3-2{Level 4-2}Level 3-3{Level 4-3}Level 3-4}}{Level 2-2{Level 3-5}}}::3";
         String[] tokens = input.split("::");
         String content = tokens[0];
         int level = Integer.parseInt(tokens[1]);
